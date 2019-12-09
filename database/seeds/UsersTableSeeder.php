22ed878388d6976559fe8dacf2ab599595a54aef
Ubuntu <ubuntu@ip-172-31-22-217.ap-southeast-1.compute.internal>
2019-12-09 01:48:02 +0000

    開発用ユーザーとテストユーザーを登録

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,7 +11,10 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $initialUser = ['name' => 'suitiku','email' => 'suitiku@gmail.com','password' => 'pc2501'];
+        $initialUser = ['name' => 'suitiku','email' => 'suitiku@gmail.com','password' => Hash::make('pc2501'),'remember_token' => str_random(10)];
         App\User::create($initialUser);
+        
+        //テストユーザー
+        factory(App\User::class,20)->create();
     }
 }
